c3f2f47c966070e4b7fe0ce16634a8b1bb644680
Shankar Vilas Patil <69981881+Shankarvpatil@users.noreply.github.com>
2021-07-05 00:46:32 +0530

    Update StockAccount_StockAccountUsingLinkedList_Stack_Queue.java

--- DAY11-12/src/com/bridgelabz/oops/StockAccount_StockAccountUsingLinkedList_Stack_Queue.java
+++ DAY11-12/src/com/bridgelabz/oops/StockAccount_StockAccountUsingLinkedList_Stack_Queue.java
@@ -1,104 +1,197 @@
 package com.bridgelabz.programs;
 
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Iterator;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
 
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
+public class StockAccount {
 
-public class JSONInventory {
+	String name;	//	name of the stock
+	int cash;	//	cash
+	int count;	
+	int total;	//	total value
+	CompanyShares[] companyShares;	//	company shares class array
+	MyStack<String> transactionsStack;	//	stack
+	MyQueue<String> transactionsQueue;	//	queue
 
 	public static void main(String[] args) {
+		StockAccount stockAccount = new StockAccount("hello");	//	class object
+		stockAccount.addOrRemoveStock();
+	}
 
-		JSONInventory inventory = new JSONInventory();	//	class object
-		//	get data from user and write to the file
-		inventory.writeData();	
-		//	read data from the file and calculate the inventory
-		inventory.readData();	
-		
+	/**
+	 * asks if user wants to add or remove stocks and gets values
+	 * else prints the details
+	 */
+	void addOrRemoveStock() {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Type 'buy' to buy stock or type 'sell' to sell stock: ");
+		String action = scanner.next();
+		if(action.equals("buy")) {
+			System.out.print("Enter amount, symbol, price of the stock: ");
+			int amount = scanner.nextInt();
+			String symbol = scanner.next();
+			int price = scanner.nextInt();
+			buy(amount, symbol, price);
+			addOrRemoveStock();
+		} else if(action.equals("sell")) {
+			System.out.print("Enter amount, symbol, price of the stock: ");
+			int amount = scanner.nextInt();
+			String symbol = scanner.next();
+			int price = scanner.nextInt();
+			sell(amount, symbol, price);
+			addOrRemoveStock();
+		}
+		else {
+			printReport();
+			System.out.println("\nTotal stock value: " + valueOf());
+			printTransactionStack();
+			printTransactionQueue();
+			writeToFile();
+			scanner.close();
+		}
 	}
 	
+	/**
+	 * @param fileName - file of stock account data
+	 * initialises queue and stock
+	 */
+	public StockAccount(String fileName) {
+		transactionsStack = new MyStack<String>();
+		transactionsQueue = new MyQueue<String>();
+		getFiledata();
+	}
+
+	/**
+	 * reads file data and puts into the array
+	 */
+	private void getFiledata() {
+		FileReader reader = null;
+		try {
+			reader = new FileReader("C:\Users\Shankar Patil\Downloads\java1.txt");
+			//	buffered reader to read the file
+			BufferedReader bufferedReader = new BufferedReader(reader);
+			name = bufferedReader.readLine();
+			cash = Integer.parseInt(bufferedReader.readLine());
+			count = Integer.parseInt(bufferedReader.readLine());
+			companyShares = new CompanyShares[10];
+			for (int i = 0; i < count; i++) {
+				String line = bufferedReader.readLine();
+				String[] lines = line.split(" ");
+				String symbol = lines[0];
+				int numberOfShares = Integer.parseInt(lines[1]);				
+				int price = Integer.parseInt(lines[2]);
+				String dateTime = lines[3];
+				companyShares[i] = new CompanyShares(symbol, numberOfShares, price, dateTime);
+			}
+			bufferedReader.close();
+		} catch (FileNotFoundException e1) {
+			e1.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @returns total value of shares
+	 */
+	public int valueOf() {
+		total = cash;
+		for (int i = 0; i < count; i++) {
+			total += companyShares[i].getValue();
+		}
+		return total;
+	}
+
+	/**
+	 * prints detailed report 
+	 */
+	public void printReport() {
+		System.out.println("\n" + name + "\n");
+		System.out.println("Symbol\tNo. of Shares\tPrice\tValue\tDate");
+		for (int i = 0; i < count; i++) {
+			CompanyShares shares = companyShares[i];
+			System.out.println(shares.getSymbol() + "\t" + shares.getNumberOfShares() + "\t\t" + shares.getPrice()
+					+ "\t" + shares.getValue() + "\t" + shares.getDateTime());
+		}			
+	}
+
+	/**
+	 * @param amount
+	 * @param symbol
+	 * @param price
+	 * buys stocks of given symbol
+	 */
+	public void buy(int amount, String symbol, int price) {
+		String dateTime = new SimpleDateFormat("dd/MM/yyyy").format(new Date());
+		CompanyShares companyShareObject = new CompanyShares(symbol, amount / price, price, dateTime);
+		companyShares[count] = companyShareObject;
+		count++;
+		transactionsStack.push("Purchased");
+		transactionsQueue.enqueue(dateTime);
+	}
+
+	/**
+	 * @param amount
+	 * @param symbol
+	 * @param price
+	 * sells stocks of given symbol
+	 */
+	public void sell(int amount, String symbol, int price) {
+		int numberOfShares = amount / price;
+		String dateTime = new SimpleDateFormat("dd/MM/yyyy").format(new Date());
+		transactionsStack.push("Sold");
+		transactionsQueue.enqueue(dateTime);
+		for (int i = 0; i < count; i++) {
+			if (companyShares[i].getSymbol().equals(symbol)) {
+				companyShares[i].setNumberOfShares(companyShares[i].getNumberOfShares() - numberOfShares);
+				companyShares[i].setDateTime(dateTime);
+				break;
+			}
+		}
+	}
 	
 	/**
-	 * Takes user input and writes data to json objcet and object to file
+	 * prints queue
 	 */
-	void writeData() {
-		Scanner scanner = new Scanner(System.in);	//	scanner to get user input
-		String[] names = new String[] {"Rice" , "Wheat" , "Pulses"};	//	inventory array
-		
-		JSONObject finalObject = new JSONObject();	//	json object to write data
-		for (String name : names) {	//	getting data for every inventory
-			System.out.print("Enter number number of types of " + name + " : ");
-			int count = scanner.nextInt();	//	types of every inventory
-			JSONArray array = new JSONArray();	//	array to store types
-			
-			for(int i = 0; i < count; i++) {	//	 object for every type
-				JSONObject jsonObject  = new JSONObject();
-				System.out.print("Enter name, weight and price: ");
-				jsonObject.put("name", scanner.next());
-				jsonObject.put("weight", scanner.nextInt());
-				jsonObject.put("price", scanner.nextInt());
-				array.add(jsonObject);
-			}			
-			finalObject.put(name , array);
+	void printTransactionQueue() {
+		System.out.println("\nTransactions Queue:");
+		while (!transactionsQueue.isEmpty()) {
+			System.out.println(transactionsQueue.dequeue());
 		}
-		scanner.close();
-		PrintWriter printWriter = null;	//	to write data to the file
+	}
+	
+	/**
+	 * prints stack
+	 */
+	void printTransactionStack() {
+		System.out.println("\nTransactions Stack:");
+		while (!transactionsStack.isEmpty()) {
+			System.out.println(transactionsStack.pop());
+		}
+	}
+	
+	/**
+	 * updates file
+	 */
+	void writeToFile() {
 		try {
-			printWriter = new PrintWriter("/home/bridgeit/Sid/myjsonfile.txt");
+			PrintWriter writer = new PrintWriter("C:\Users\Shankar Patil\Downloads\java1.txt");
+			writer.write(name + "\n" + cash + "\n" + count + "\n");
+			for (int i = 0; i < count; i++) {
+				CompanyShares share = companyShares[i];
+				writer.write(share.getSymbol() + " " + share.getNumberOfShares()
+						+ " " + share.getPrice() + " " + share.getDateTime() + "\n");
+			} 
+			writer.close();
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		printWriter.write(finalObject.toJSONString());
-		printWriter.close();
-	}
-
-	/**
-	 * reads data from json file and 
-	 * prints inventory value of Rice, Pulses and Wheat
-	 */
-	void readData() {
-		JSONArray array = new JSONArray();	//	array to get json data from file
-		JSONParser parser = new JSONParser();	//	parser to parse data from file
-		JSONObject object;	// json object to store every type of inventory
-		
-		try {
-			object = (JSONObject) parser.parse(new FileReader("/home/bridgeit/Sid/myjsonfile.txt"));
-			array = (JSONArray) object.get("Rice");
-			System.out.println("Rice inventory value: " + getValue(array));
-			array = (JSONArray) object.get("Pulses");
-			System.out.println("Pulses inventory value: " + getValue(array));
-			array = (JSONArray) object.get("Wheat");
-			System.out.println("Wheat inventory value: " + getValue(array));
-		}
-		catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (org.json.simple.parser.ParseException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	/**
-	 * @param array - json array
-	 * @returns value of given inventory array
-	 */
-	long getValue(JSONArray array){
-		Iterator<?> iterator = array.iterator();	//	iterator to iterate
-		long value = 0;	//	inventory value
-		while(iterator.hasNext()) {
-			JSONObject obj = (JSONObject)iterator.next();
-			long weight = (long)obj.get("weight");
-			long price = (long)obj.get("price");
-			value = value + weight * price;	//	adding value
-		}
-		return value;
 	}
 }
