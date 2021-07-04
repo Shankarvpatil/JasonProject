c82d05c0d8ff8b50e032f486da07bb55ee6fffb3
Shankar Vilas Patil <69981881+Shankarvpatil@users.noreply.github.com>
2021-07-04 19:48:21 +0530

    Update StockAccountUtility.java

--- DAY11-12/src/com/bridgelabz/oops/StockAccountUtility.java
+++ DAY11-12/src/com/bridgelabz/oops/StockAccountUtility.java
@@ -1,73 +1,75 @@
-package com.Bridgelabz.Program;
+package com.bridgelabz.oops;
 
+import java.io.File;
 import java.io.IOException;
 
 import org.codehaus.jackson.JsonParseException;
 import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
 
-import com.Bridgelabz.helper.JsonUtil;
-import com.Bridgelabz.model.TransactionModel;
+import com.bridgelabz.model.StockModel;
+import com.bridgelabz.utility.OOPsUtility;
 
-public class StockAccountUtility {
+public class Stock {
 
-	/**
-	 * Purpose: this method will read the customer accounts and returns all accounts
-	 * 
-	 * @param fileName this is path of file is provided by user
-	 * @return returns the object of mapped class
-	 * @throws JsonParseException   throws exception if parsing fails
-	 * @throws JsonMappingException throws exception if mapping to model is fails
-	 * @throws IOException          throws exception if input output operation fails
-	 */
-	public static Object stockAccount(String fileName, Object object)
-			throws JsonParseException, JsonMappingException, IOException {
-		return JsonUtil.readMapper(fileName, object.getClass());
-	}
+	public static void main(String[] args) throws JsonParseException, JsonMappingException, IOException {
+		String path = "/home/admin1/eclipse-workspace/BridgeLabzFellowshipPrograms/src/com/bridgelabz/jsonfiles/stock.json";
+		String outputPath = "/home/admin1/eclipse-workspace/BridgeLabzFellowshipPrograms/src/com/bridgelabz/jsonfiles/stockOutput.json";
 
-	/**
-	 * Purpose: this method will calculate total value of shares
-	 * 
-	 * @return returns the total value of shares
-	 */
-	public static double valueOf() {
-		return 0;
+		ObjectMapper mapper = new ObjectMapper();
 
-	}
+		StockModel data = mapper.readValue(new File(path), StockModel.class);
+		System.out.println("-----------------------------Stock Report--------------------------------");
+		System.out.println("Stock Name: " + data.getParle().get(0).getStock_name());
+		System.out.println("Stock Price: " + data.getParle().get(0).getShare_price());
+		System.out.println("Stock Number of Shares: " + data.getParle().get(0).getNumber_of_shares());
+		System.out.println();
+		int totalValueOfParle = OOPsUtility.calculateTotalValueOfStock(data.getParle().get(0).getNumber_of_shares(),
+				data.getParle().get(0).getShare_price());
+		System.out.println("Total value of Stock Parle -->" + totalValueOfParle);
 
-	/**
-	 * Purpose: this method is used to buy shares
-	 * 
-	 * @param companySymbol
-	 * @param customerId
-	 * @param model
-	 * @param numOfCompanyShareToBuy
-	 * @return
-	 */
-	public static Object buyShare(TransactionModel model, int customerId, int companySymbol,
-			int numOfCompanyShareToBuy) {
+		data.getParle().get(0).setTotalValueOfStock(totalValueOfParle);
 
-		return model;
-	}
+		System.out.println();
+		System.out.println("Stock Name: " + data.getMarie().get(0).getStock_name());
+		System.out.println("Stock Price: " + data.getMarie().get(0).getShare_price());
+		System.out.println("Stock Number of Shares: " + data.getParle().get(0).getNumber_of_shares());
+		System.out.println();
+		int totalValueOfMarie = OOPsUtility.calculateTotalValueOfStock(data.getMarie().get(0).getNumber_of_shares(),
+				data.getMarie().get(0).getShare_price());
+		System.out.println("Total value of Stock Marie -->" + totalValueOfMarie);
+		data.getMarie().get(0).setTotalValueOfStock(totalValueOfMarie);
 
-	/**
-	 * Purpose: this method is used to sell shares
-	 */
-	public static void sellShare() {
+		System.out.println();
+		System.out.println("Stock Name: " + data.getOreo().get(0).getStock_name());
+		System.out.println("Stock Price: " + data.getOreo().get(0).getShare_price());
+		System.out.println("Stock Number of Shares: " + data.getOreo().get(0).getNumber_of_shares());
+		System.out.println();
+		int totalValueOfOreo = OOPsUtility.calculateTotalValueOfStock(data.getOreo().get(0).getNumber_of_shares(),
+				data.getOreo().get(0).getShare_price());
+		System.out.println("Total value of Stock Oreo -->" + totalValueOfOreo);
+		data.getOreo().get(0).setTotalValueOfStock(totalValueOfOreo);
 
-	}
+		System.out.println();
+		System.out.println("Stock Name: " + data.getTiger().get(0).getStock_name());
+		System.out.println("Stock Price: " + data.getTiger().get(0).getShare_price());
+		System.out.println("Stock Number of Shares: " + data.getTiger().get(0).getNumber_of_shares());
+		System.out.println();
+		int totalValueOfTiger = OOPsUtility.calculateTotalValueOfStock(data.getTiger().get(0).getNumber_of_shares(),
+				data.getTiger().get(0).getShare_price());
+		System.out.println("Total value of Stock Tiger -->" + totalValueOfTiger);
+		data.getTiger().get(0).setTotalValueOfStock(totalValueOfTiger);
 
-	/**
-	 * @param filename file path where we are going to save json
-	 * @param object   object is used to maps the json
-	 * @return returns success if all operation performs well
-	 * @throws JsonParseException   throws exception if parsing fails
-	 * @throws JsonMappingException throws exception if mapping to model is fails
-	 * @throws IOException          throws exception if input output operation fails
-	 */
-	public static String save(String filename, Object object)
-			throws JsonParseException, JsonMappingException, IOException {
-		JsonUtil.writeMapper(filename, object);
-		return "Saving successful";
+		int totalValueOfAllStock = totalValueOfParle + totalValueOfMarie + totalValueOfOreo + totalValueOfTiger;
+
+		data.setTotalValueOfAllStock(totalValueOfAllStock);
+		System.out.println();
+		System.out.println("Total count of all stocks -->" + totalValueOfAllStock);
+		System.out.println();
+		System.out.println("-----------------------------Stock Report--------------------------------");
+		// code for writing all details into new file
+		mapper.writeValue(new File(outputPath), data);
+		System.out.println("\nWrite into file is completed!!!");
 	}
 
 }
